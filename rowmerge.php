000ebbfea4825fd76cc49d873addf6f87f5de379
ravihandsintechnology <ravi@handsintechnology.com>
2019-02-21 10:46:16 +0530

    col merge and row merge

---
+++ rowmerge.php
@@ -0,0 +1,92 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
+date_default_timezone_set('Asia/Kolkata');
+
+define('EOL',(PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
+
+/** Include PHPExcel */
+require_once dirname(__FILE__) . '/Classes/PHPExcel.php';
+
+// Create new PHPExcel object
+echo date('H:i:s') , " Create new PHPExcel object" , EOL;
+$objPHPExcel = new PHPExcel();
+
+// Set document properties
+echo date('H:i:s') , " Set document properties" , EOL;
+$objPHPExcel->getProperties()->setCreator("Ravi Varma")
+							 ->setLastModifiedBy("Ravi Varma")
+							 ->setTitle("Excel Merge Document")
+							 ->setSubject("Excel Merge Document")
+							 ->setDescription("Example using PHPExcel for generating excel sheets like colspan.")
+							 ->setKeywords("office PHPExcel php")
+							 ->setCategory("Test result file");
+
+
+// Add some data
+echo date('H:i:s') , " Add some data" , EOL;
+$objPHPExcel->setActiveSheetIndex(0);
+$objPHPExcel->getActiveSheet()->setCellValue('A1', 'School Name');
+$objPHPExcel->getActiveSheet()->mergeCells('A1:A2');
+
+$i=0;
+$column = 'B';
+while($i<10)
+{
+    $objPHPExcel->getActiveSheet()->setCellValue($column.'1', 'dddddd');
+    $mergeRange = $column.'1:';
+    $objPHPExcel->getActiveSheet()->setCellValue($column.'2', 'Student Name');
+    $column++;
+    $mergeRange .= $column.'1';
+    $objPHPExcel->getActiveSheet()->setCellValue($column.'2', 'Grade');
+    $objPHPExcel->getActiveSheet()->mergeCells($mergeRange);
+    $column++;
+    $i++;
+}
+// Rename worksheet
+echo date('H:i:s') , " Rename worksheet" , EOL;
+$objPHPExcel->getActiveSheet()->setTitle('Excel Merge Document');
+
+
+// Set active sheet index to the first sheet, so Excel opens this as the first sheet
+$objPHPExcel->setActiveSheetIndex(0);
+
+
+// Save Excel 2007 file
+echo date('H:i:s') , " Write to Excel2007 format" , EOL;
+$callStartTime = microtime(true);
+
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+$objWriter->save(str_replace('.php', '.xlsx', __FILE__));
+$callEndTime = microtime(true);
+$callTime = $callEndTime - $callStartTime;
+
+echo date('H:i:s') , " File written to " , str_replace('.php', '.xlsx', pathinfo(__FILE__, PATHINFO_BASENAME)) , EOL;
+echo 'Call time to write Workbook was ' , sprintf('%.4f',$callTime) , " seconds" , EOL;
+// Echo memory usage
+echo date('H:i:s') , ' Current memory usage: ' , (memory_get_usage(true) / 1024 / 1024) , " MB" , EOL;
+
+
+// Save Excel 95 file
+echo date('H:i:s') , " Write to Excel5 format" , EOL;
+$callStartTime = microtime(true);
+
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+$objWriter->save(str_replace('.php', '.xls', __FILE__));
+$callEndTime = microtime(true);
+$callTime = $callEndTime - $callStartTime;
+
+echo date('H:i:s') , " File written to " , str_replace('.php', '.xls', pathinfo(__FILE__, PATHINFO_BASENAME)) , EOL;
+echo 'Call time to write Workbook was ' , sprintf('%.4f',$callTime) , " seconds" , EOL;
+// Echo memory usage
+echo date('H:i:s') , ' Current memory usage: ' , (memory_get_usage(true) / 1024 / 1024) , " MB" , EOL;
+
+
+// Echo memory peak usage
+echo date('H:i:s') , " Peak memory usage: " , (memory_get_peak_usage(true) / 1024 / 1024) , " MB" , EOL;
+
+// Echo done
+echo date('H:i:s') , " Done writing files" , EOL;
+echo 'Files have been created in ' , getcwd() , EOL;
